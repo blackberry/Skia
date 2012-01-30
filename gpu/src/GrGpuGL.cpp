5ef1dd512a02d29a8dd0a4e9b7e4102974ef7b44
Artem Simonov <asimonov@rim.com>
2012-01-30 11:39:34 -0500

    Added copyright notices to all files touched by RIM.

--- gpu/src/GrGpuGL.cpp
+++ gpu/src/GrGpuGL.cpp
@@ -1,5 +1,6 @@
 /*
     Copyright 2011 Google Inc.
+    Copyright (C) 2011 Research In Motion Limited. All rights reserved.
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
--- src/core/SkScan_Antihair.cpp
+++ src/core/SkScan_Antihair.cpp
@@ -1,6 +1,7 @@
 /* libs/graphics/sgl/SkScan_Antihair.cpp
 **
 ** Copyright 2011, The Android Open Source Project
+** Copyright (C) 2011 Research In Motion Limited. All rights reserved.
 **
 ** Licensed under the Apache License, Version 2.0 (the "License"); 
 ** you may not use this file except in compliance with the License. 
